456e179407deb628c607ba494244fe347a6c22c9
nakapon9517 <maksminakapon@gmail.com>
2020-08-15 19:10:38 +0900

    fix

--- learn_rn/src/views/nakazato/index.tsx
+++ learn_rn/src/views/nakazato/index.tsx
@@ -1,11 +1,25 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, SafeAreaView, StyleSheet } from "react-native";
+import { View, Image, Text, SafeAreaView, StyleSheet } from "react-native";
+import { Icon, Input } from "react-native-elements";
 
 export default function Nakazato() {
   return (
     <SafeAreaView style={styles.container}>
-      <View>
+      <View style={styles.component}>
         <Text>Nakazato</Text>
+      </View>
+      <View style={styles.component}>
+        <Icon type="font-awesome" name="home"></Icon>
+        <Icon type="font-awesome" name="user"></Icon>
+      </View>
+      <View style={styles.component}>
+        <Input style={styles.input_test} placeholder="test"></Input>
+      </View>
+      <View style={styles.component}>
+        <Image
+          style={styles.image}
+          source={require("../../statics/images/sample_reactnative.png")}
+        ></Image>
       </View>
     </SafeAreaView>
   );
@@ -17,5 +31,25 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  component: {
+    borderTopWidth: 2,
+    borderTopColor: "gray",
+    borderTopLeftRadius: 8,
+    borderTopRightRadius: 8,
+    borderBottomLeftRadius: 8,
+    borderBottomRightRadius: 8,
+    height: "20%",
+    width: "100%",
+  },
+  input_test: {
+    width: 500,
+    height: 100,
+    borderWidth: 3,
+    borderColor: "gray",
+  },
+  image: {
+    width: 100,
+    height: 100,
   },
 });
