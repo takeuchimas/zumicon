14de77fd77901f1c12413f6ea4892d3aa1e07a8f
田中健太 <dorakenn1215@gmail.com>
2020-10-04 02:52:15 +0900

    一旦コミットします

--- relux/src/views/_spajam/home/home.tsx
+++ relux/src/views/_spajam/home/home.tsx
@@ -5,6 +5,18 @@ type TextProps = {
   text: string;
 }
 
+// 検索ボックス
+export const TextBoxRender: React.FC<TextProps> = ({ text }) => {
+  return (
+    <Text
+      style={[styles.text_template, styles.title]}
+    >
+      {text}
+    </Text>
+  );
+}
+
+// 画面タイトル
 export const TitleRender: React.FC<TextProps> = ({ text }) => {
   return (
     <Text
@@ -15,6 +27,7 @@ export const TitleRender: React.FC<TextProps> = ({ text }) => {
   );
 }
 
+// 項目タイトル
 export const ItemBoxRender: React.FC<TextProps> = ({ text }) => {
   return (
     <Text
@@ -25,6 +38,7 @@ export const ItemBoxRender: React.FC<TextProps> = ({ text }) => {
   );
 }
 
+// 項目
 export const ItemRender: React.FC<TextProps> = ({ text }) => {
   return (
     <Text
@@ -36,25 +50,41 @@ export const ItemRender: React.FC<TextProps> = ({ text }) => {
 }
 
 export default function Home() {
+
+  // 初期化時に一度だけ呼ばれる
+  useEffect(() => {
+    // APIを呼び出して、関数セット？
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
         <TitleRender text='HOME' />
 
         <ItemBoxRender text='おすすめ' />
+        {/* おすすめ項目を出す */}
+        <ItemRender text='へび' />
+        <ItemRender text='ねこ' />
 
-        <ItemRender text='履歴' />
+        <ItemBoxRender text='履歴' />
+        {/* ユーザーの閲覧履歴を出す */}
+        <ItemRender text='三毛猫' />
 
       </View>
     </SafeAreaView>
   );
 }
 
+// TypeScript関数群
+let function1 = () => {
+
+};
+
+// スタイル
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "flex-start",
     // justifyContent: "center",
   },
   text_template: {
@@ -64,11 +94,12 @@ const styles = StyleSheet.create({
   // 画面タイトル
   title: {
     fontSize: 40,
+    paddingLeft: 0,
     alignSelf: 'center',
   },
   // 項目のタイトル
   item_box: {
-    fontSize: 20,
+    fontSize: 25,
   },
   // 項目
   item: {
